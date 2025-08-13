1a0b656ce2fbf2f3e9e47cd88856371979a843ac
smbeane5235 <smbeane5235@gmail.com>
2025-08-13 15:26:29 -0400

    Update grid to work with offscreen canvas

--- testing/lib/components/grid.py
+++ testing/lib/components/grid.py
@@ -9,11 +9,35 @@ class Grid:
         self.grid_size = grid_size #will allow for text to be 3x5, 5x7, or larger?
         self.color = color
 
-        self.full_content = content #what should go on the display, used for situations such as if a name is too long to actually fit on the display
-        self.display_content = [] #content currently on the display
-        self.offscreen_content = self.initialize_offscreen_content(content) #content on the offscreen canvas
+        self.full_content = content
+        self.offscreen_content = self._init_content(content)
 
-    def initialize_offscreen_content(self, full_content: List[str]) -> List[List[str]]:
+    def initial_render(self, canvas) -> None:
+       for y_offset, line in enumerate(self.offscreen_content):
+           for x_offset, character in enumerate(line):
+               x_pos = self.x + x_offset * 4
+               y_pos = self.y + y_offset * 6
+               set_character(canvas, character, (x_pos, y_pos), self.color) 
+
+    def update_and_render(self, canvas: Any, updated_content: List[str]) -> None:
+        updated_cells = self._update_content(updated_content)
+
+        if len(updated_cells) == 0:
+            return
+
+        if updated_cells == [(-1, -1)]:
+            self.initial_render(canvas)
+            return
+        
+        for x, y in updated_cells:
+            character = self.offscreen_content[y][x]
+            position = (self.x + x * 4, self.y + y * 6)
+
+            set_character(canvas, character, position, self.color)
+
+        self.full_content = updated_content
+
+    def _init_content(self, full_content: List[str]) -> List[List[str]]:
         offscreen_content = []
         
         for line in full_content[:self.y_len]: 
@@ -22,20 +46,11 @@ class Grid:
 
         return offscreen_content
 
-    def render_offscreen(self, canvas): 
-        for y_offset, line in enumerate(self.offscreen_content):
-            for x_offset, character in enumerate(line):
-                x_pos = self.x + x_offset * 4
-                y_pos = self.y + y_offset * 6 
-                set_character(canvas, character, (x_pos, y_pos), self.color)
-       
-        self.swap_contents()
-    
-    def update_offscreen_content(self, updated_content: List[str]) -> List[Tuple[int, int]]:
+    def _update_content(self, updated_content: List[str]) -> List[Tuple[int, int]]:
         updated_cells = []
 
         if self.offscreen_content == []:
-            self.offscreen_content = self.initialize_offscreen_content(updated_content)
+            self.offscreen_content = self._init_content(updated_content)
             return [(-1, -1)]
         
         for y_index in range(self.y_len):
@@ -52,33 +67,9 @@ class Grid:
         
         return updated_cells
     
-    def update_offscreen(self, canvas: Any, updated_content: List[str]) -> None:
-        updated_cells = self.update_offscreen_content(updated_content)
-
-        if len(updated_cells) == 0:
-            return
-        
-        if(updated_cells[0] == (-1, -1)):
-            self.render_offscreen(canvas)
-            return
-
-        for x, y in updated_cells:
-            character = self.offscreen_content[y][x]
-            position = (self.x + x * 4, self.y + y * 6)
-
-            set_character(canvas, character, position, self.color)
-        
-        self.swap_contents()
-        self.content = updated_content
-
     #TODO
     def scroll_offscreen(self, canvas: Any) -> None:
         pass
-
-    def swap_contents(self) -> None:
-        curr_content = self.display_content
-        self.display_content = self.offscreen_content
-        self.offscreen_content = curr_content
 
 def set_character(canvas: Any, character: str, position: Tuple[int, int], rgb: Tuple[int, int, int] = WHITE) -> None:
     character = character.lower()
